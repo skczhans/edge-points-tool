2987e788a3b719820b4ba90e914bb1b7449aa364
skczhans <54059062+skczhans@users.noreply.github.com>
2023-09-10 22:40:38 +0800

    Update 初始化环境.bat

--- 初始化环境.bat
+++ 初始化环境.bat
@@ -1,4 +1,4 @@
 @echo off
 python.exe -m pip install --upgrade pip
-pip install selenium==4.11.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install selenium==4.11.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pause
